c4aa4cd361b09e00d3893f9f3accea88cebb1002
struikrovi1 <sabina20028@gmail.com>
2021-12-25 02:14:02 +0400

    first commit

--- FirstAmc/Controllers/HomeController.cs
+++ FirstAmc/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace FirstAmc.Controllers
         {
             ViewData["hehehehe"] = "heehehe";
            
-            return View();
+            return View(); 
         } 
 
         public IActionResult About()
